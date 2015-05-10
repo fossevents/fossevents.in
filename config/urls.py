96b2f305160f56e26e98de3b9b7af62f7d4f8321
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-05-10 12:40:45 +0530

    Disable public registration urls and views

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     # User management
-    url(r'^users/', include("fossevents.users.urls", namespace="users")),
-    url(r'^accounts/', include('allauth.urls')),
+    # url(r'^users/', include("fossevents.users.urls", namespace="users")),
+    # url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
 
