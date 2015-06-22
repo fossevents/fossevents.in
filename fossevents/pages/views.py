5c931b3d8226edfe971b3b7959179acadd06b386
kushagra43 <kushagra43@gmail.com>
2015-06-22 16:08:10 +0530

    dummy data

--- fossevents/pages/views.py
+++ fossevents/pages/views.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from fossevents.events import services as event_services
-
+from datetime import datetime
+from fossevents.events.models import Event
 
 def homepage(request):
+# dummy data
+    Event.objects.create(name="google",description="test123",homepage="www.google.com",is_published=True,auth_token=12345,owner_email="kushagra43@gmail.com",start_date=datetime.today(),end_date=datetime.today())			
+# dummy end
+
     ctx = {
         'events': event_services.get_public_event_listings()
     }
