bd6917cb678c8fd4c1383298db85e4c6edb2e9c8
kushagra43 <kushagra43@gmail.com>
2015-06-24 13:02:31 +0530

    bug fixes 2

--- fossevents/events/management/commands/sample_data.py
+++ fossevents/events/management/commands/sample_data.py
@@ -4,21 +4,23 @@ from django.utils import timezone
 
 class Command(BaseCommand):
 
-    def handle(self, * args, * * options):
-    print(' Creating events')
-    self.events = self.create_events()
-    print(' events created')
 
-def create_events(self):
-    for count in range(0, 20):
-    Event.objects.create( * * {
-        'name': "name %d" % count,
-        'description': "%d description" % count,
-        'start_date': timezone.now(),
-        'end_date': timezone.now(),
-        'homepage': "www.google.com",
-        'is_published': True,
-        'auth_token': "12345",
-        'owner_email': "kushagra43@gmail.com",
-    })
-    return
+    def handle(self, * args, * * options):
+        print(' Creating events')
+        self.events = self.create_events()
+        print(' events created')
+
+
+    def create_events(self):
+        for count in range(0, 20):
+    	   Event.objects.create( * * {
+ 	        'name': "name %d" % count,
+            'description': "%d description" % count,
+            'start_date': timezone.now(),
+            'end_date': timezone.now(),
+            'homepage': "www.google.com",
+            'is_published': True,
+            'auth_token': "12345",
+            'owner_email': "kushagra43@gmail.com",
+            })
+        return
