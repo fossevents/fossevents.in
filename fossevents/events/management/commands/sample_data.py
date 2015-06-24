663c989906263401f97659bcb6d4fa0efe42bb57
kushagra43 <kushagra43@gmail.com>
2015-06-24 14:01:24 +0530

    flake fix

--- fossevents/events/management/commands/sample_data.py
+++ fossevents/events/management/commands/sample_data.py
@@ -1,26 +1,25 @@
-from django.core.management.base import BaseCommand, CommandError
-from fossevents.events.modelsimport Event
+from django.core.management.base import BaseCommand
+from fossevents.events.models import Event
 from django.utils import timezone
+
 
 class Command(BaseCommand):
 
-
-    def handle(self, * args, * * options):
+    def handle(self, *args, **options):
         print(' Creating events')
         self.events = self.create_events()
         print(' events created')
 
-
     def create_events(self):
         for count in range(0, 20):
-    	   Event.objects.create( * * {
- 	        'name': "name %d" % count,
-            'description': "%d description" % count,
-            'start_date': timezone.now(),
-            'end_date': timezone.now(),
-            'homepage': "www.google.com",
-            'is_published': True,
-            'auth_token': "12345",
-            'owner_email': "kushagra43@gmail.com",
+            Event.objects.create(**{
+                'name': "name %d" % count,
+                'description': "%d description" % count,
+                'start_date': timezone.now(),
+                'end_date': timezone.now(),
+                'homepage': "www.google.com",
+                'is_published': True,
+                'auth_token': "12345",
+                'owner_email': "kushagra43@gmail.com",
             })
         return
--- fossevents/pages/views.py
+++ fossevents/pages/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts
-from fossevents.events
-import render
-import services as event_services
+from django.shortcuts import render
+from fossevents.events import services as event_services
+
 
 def homepage(request):
     ctx = {
         'events': event_services.get_public_event_listings()
     }
-return render(request, 'pages/home.html', ctx)
+    return render(request, 'pages/home.html', ctx)
