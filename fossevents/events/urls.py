80cbc29d8dc910bcd5f85a39eadcf78ba8081b29
kushagra43 <kushagra43@gmail.com>
2015-07-08 19:26:08 +0530

    added rss feed

--- fossevents/events/urls.py
+++ fossevents/events/urls.py
@@ -5,6 +5,12 @@ from django.conf.urls import url
 
 from . import views
 
+from .feeds import ArchiveFeed
+
 urlpatterns = [
     url(r'^(?P<slug>[-\w]+)-(?P<pk>[-\w]+)/$', views.event_detail, name="detail"),
+]
+
+urlpatterns += [
+    url(r'^feed/archive/$', ArchiveFeed()),
 ]
