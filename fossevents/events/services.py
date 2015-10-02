6bb2caae2a2ed9fcf0a0614fc52fe25a90fd31e3
Vipul Sharma <vipul.sharma20@gmail.com>
2015-10-02 17:05:52 +0530

    add basic search functionality
    For #17
    Closes #24

--- fossevents/events/services.py
+++ fossevents/events/services.py
@@ -3,6 +3,8 @@
 from . import models
 
 
-def get_public_event_listings():
+def get_public_event_listings(search_term=None):
     qs = models.Event.objects.filter(is_published=True)
+    if search_term:
+        qs = qs.filter(name__icontains=search_term)
     return qs
--- fossevents/views.py
+++ fossevents/views.py
@@ -5,7 +5,8 @@ from fossevents.events import services as event_services
 
 
 def homepage(request):
+    search_term = request.GET.get('q', None)
     ctx = {
-        'events': event_services.get_public_event_listings()
+        'events': event_services.get_public_event_listings(search_term=search_term)
     }
     return render(request, 'home.html', ctx)
