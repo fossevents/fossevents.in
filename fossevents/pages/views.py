c01a0a3cc4c59747228e63fc677a594f4a7f5312
Vipul Sharma <vipul.sharma20@gmail.com>
2015-10-03 17:42:51 +0530

    #17 add basic search functionality

--- fossevents/pages/views.py
+++ fossevents/pages/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 from fossevents.events import services as event_services
 from fossevents.events.models import Event
 
+
 def homepage(request):
     ctx = {
         'events': event_services.get_public_event_listings()
     }
     return render(request, 'pages/home.html', ctx)
+
 
 def search(request):
     if 'q' in request.GET:
