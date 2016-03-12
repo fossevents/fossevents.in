106f8d1063ac96f0ff81a7d439163ab99553e288
Saurabh Kumar <me+github@saurabh-kumar.com>
2016-03-13 01:45:44 +0530

    Update search UI

--- fossevents/static/js/project.js
+++ fossevents/static/js/project.js
@@ -1,1 +1,10 @@
 /* Project specific Javascript goes here. */
+
+$(document).keydown(function(e){
+    // Focus search bar when `/` is pressed
+    var forward_slash = 191;
+    if (e.keyCode == forward_slash) {
+       $('input[name=q]').focus();
+       return false;
+    }
+});
