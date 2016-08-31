88a328802e5d21e92360632e87d30450aef6a927
Mayank Jain <jainmickey93@gmail.com>
2016-09-01 00:45:56 +0530

    feat(forms): Add placeholder in event creation form.

--- fossevents/events/forms.py
+++ fossevents/events/forms.py
@@ -1,17 +1,26 @@
-from django.forms import ModelForm, ValidationError
+from django import forms
+from django_markdown.widgets import MarkdownWidget
 
 from .models import Event
 
 
-class EventCreateForm(ModelForm):
+class EventCreateForm(forms.ModelForm):
     class Meta:
         model = Event
         exclude = ('auth_token', 'is_published')
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Title'}),
+            'description': MarkdownWidget(attrs={'placeholder': 'Enter description here'}),
+            'start_date': forms.DateTimeInput(attrs={'placeholder': 'yyyy-mm-dd'}),
+            'end_date': forms.DateTimeInput(attrs={'placeholder': 'yyyy-mm-dd'}),
+            'homepage': forms.URLInput(attrs={'placeholder': 'http://example.com'}),
+            'owner_email': forms.EmailInput(attrs={'placeholder': 'user@example.com'}),
+        }
 
     def clean_end_date(self):
         start_date = self.cleaned_data.get('start_date')
         end_date = self.cleaned_data.get('end_date')
         if end_date:
             if end_date < start_date:
-                raise ValidationError('End date should be greater than start date.')
+                raise forms.ValidationError('End date should be greater than start date.')
         return end_date
